4ed703e42f1a59a743108d0271ecfded893a181b
Ymerald <72456735+Ymerald@users.noreply.github.com>
2021-09-06 13:08:30 -0700

    Add files via upload

---
+++ AllTack/Main.cs
@@ -0,0 +1,124 @@
+﻿using MelonLoader;
+using Harmony;
+using Assets.Scripts.Unity.UI_New.InGame.Races;
+using Assets.Scripts.Simulation.Towers.Weapons;
+using Assets.Scripts.Simulation;
+using Assets.Scripts.Unity.UI_New.InGame;
+using Assets.Scripts.Unity.UI_New.Main;
+using Assets.Scripts.Simulation.Bloons;
+using Assets.Scripts.Models.Towers;
+
+using Assets.Scripts.Unity;
+
+
+
+using Assets.Scripts.Simulation.Towers;
+
+using Assets.Scripts.Utils;
+
+using Il2CppSystem.Collections;
+using Assets.Scripts.Unity.UI_New.Popups;
+using Assets.Scripts.Unity.Bridge;
+using Assets.Scripts.Models.Towers.Behaviors;
+using Assets.Scripts.Simulation.Objects;
+using Assets.Scripts.Models;
+using TMPro;
+using Assets.Scripts.Models.Towers.Behaviors.Attack;
+using System;
+using Assets.Scripts.Simulation.Towers.Behaviors;
+using Assets.Scripts.Simulation.Towers.Projectiles.Behaviors;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Main.Scenes;
+using Assets.Scripts.Models.GenericBehaviors;
+using Assets.Scripts.Simulation.Towers.Pets;
+using UnityEngine;
+using BTD_Mod_Helper.Extensions;
+using Assets.Scripts.Models.Bloons.Behaviors;
+using BTD_Mod_Helper;
+using Il2CppSystem.Threading;
+using Il2CppSystem.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Assets.Scripts.Models.Towers.Behaviors.Attack.Behaviors;
+using Assets.Scripts.Models.Map;
+using Assets.Scripts.Models.Towers.Behaviors.Emissions;
+using Assets.Scripts.Models.Towers.Projectiles.Behaviors;
+
+namespace btd6ai
+{
+    public class Main : BloonsTD6Mod
+    {
+        public override void OnTitleScreen()
+        {
+            base.OnTitleScreen();
+
+            Console.WriteLine("tackifying...");
+
+            string[] names = new string[Game.instance.model.towers.Length];
+            int i = 0;
+            foreach (var tower in Game.instance.model.towers)
+            {
+                names[i] = tower.name;
+                i++;
+            }
+
+            foreach (var tower in Game.instance.model.towers)
+            {
+                if (tower.HasBehavior<AttackModel>())
+                {
+                    foreach (var attackModel in tower.GetBehaviors<AttackModel>())
+                    {
+                        attackModel.RemoveBehaviors<RotateToTargetModel>();
+                        Console.WriteLine(tower.name);
+
+                        var weapons = attackModel.weapons;
+                        foreach (var weapon in weapons)
+                        {
+                            if (weapon.projectile.HasBehavior<CreateTowerModel>())
+                            {
+                                string name = weapon.projectile.GetBehavior<CreateTowerModel>().tower.name;
+                                int index = Array.IndexOf(names, name);
+                                weapon.projectile.GetBehavior<CreateTowerModel>().tower = Game.instance.model.towers.ElementAt(index);
+                            }
+                            if (weapon.projectile.HasBehavior<CreateTypedTowerModel>())
+                            {
+                                string name = weapon.projectile.GetBehavior<CreateTypedTowerModel>().boomTower.name;
+                                int index = Array.IndexOf(names, name);
+                                weapon.projectile.GetBehavior<CreateTypedTowerModel>().boomTower = Game.instance.model.towers.ElementAt(index);
+
+                                name = weapon.projectile.GetBehavior<CreateTypedTowerModel>().coldTower.name;
+                                index = Array.IndexOf(names, name);
+                                weapon.projectile.GetBehavior<CreateTypedTowerModel>().coldTower = Game.instance.model.towers.ElementAt(index);
+
+                                name = weapon.projectile.GetBehavior<CreateTypedTowerModel>().crushingTower.name;
+                                index = Array.IndexOf(names, name);
+                                weapon.projectile.GetBehavior<CreateTypedTowerModel>().crushingTower = Game.instance.model.towers.ElementAt(index);
+
+                                name = weapon.projectile.GetBehavior<CreateTypedTowerModel>().energyTower.name;
+                                index = Array.IndexOf(names, name);
+                                weapon.projectile.GetBehavior<CreateTypedTowerModel>().energyTower = Game.instance.model.towers.ElementAt(index);
+                            }
+
+                            float count = 1;
+                            weapon.projectile.pierce /= 2;
+                            if (weapon.emission.IsType<ArcEmissionModel>())
+                            {
+                                count = weapon.emission.Cast<ArcEmissionModel>().count;
+
+                                if (weapon.emission.Cast<ArcEmissionModel>().angle == 360)
+                                {
+                                    count /= 6;
+                                }
+                            }
+                            weapon.emission = new ArcEmissionModel("ArcEmmissionModel_", (int)(6 * count), 0, 360, null, false, false);
+                            weapon.animateOnMainAttack = false;
+                            weapon.ejectX = 0;
+                            weapon.ejectY = 0;
+                            weapon.ejectZ = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
